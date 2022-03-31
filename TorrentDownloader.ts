2bf0ad4955ec565e7b506f05112762404a7fb50f
Yahya Fati <yfati037@gmail.com>
2022-03-31 10:47:51 +0300

    changed timeout on axios

--- TorrentDownloader.ts
+++ TorrentDownloader.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { HTMLElement, parse } from "node-html-parser";
 
 const axiosInstance = axios.create({
-    timeout: 14000,
+    timeout: 15000,
 });
 
 export interface Torrent {
